135a3bf6546a8e60790289bf4a58d1ba2695ea3d
visala123 <visala.k@algorims.com>
2025-04-10 12:35:31 +0800

    add staging and .github folders to feature file

--- staging/main.tf
+++ staging/main.tf
@@ -24,10 +24,3 @@ module "eks" {
   key_pair                = "keypair"
 }
 
-/*module "alb" {
-  source = "./modules/alb"
-  sg_id = module.sg.sg_id
-  subnets = module.vpc.subnet_ids
-  vpc_id = module.vpc.vpc_id
-  instances = module.ec2.instances
-}*/
